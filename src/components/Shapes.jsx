0b16098220301d6e35b89dbd50ae16e1a9dc9668
AliAbdur1 <AliAbdur171@gmail.com>
2025-09-16 19:47:18 -0400

    irridesence

--- src/components/Shapes.jsx
+++ src/components/Shapes.jsx
@@ -4,6 +4,9 @@ import gsap from 'gsap';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import GUI from 'lil-gui';
 import SphereImageSource from '/src/images/Rock052_1K-JPG/Rock052_1K-JPG_Color.jpg';
+import { HDRLoader } from 'three/examples/jsm/loaders/HDRLoader.js'
+import { transmission } from 'three/tsl';
+console.log(HDRLoader);
 
 const sizes = {
   width: window.innerWidth,
@@ -60,15 +63,24 @@ function Shapes() {
     // Example geometry (so something shows up)
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const cubecolortexture = textureLoader.load('/src/images/11-materials/static/textures/door/color.jpg');
+    cubecolortexture.colorSpace = THREE.SRGBColorSpace;
     const cubealphatexture = textureLoader.load('/src/images/11-materials/static/textures/door/alpha.jpg');
+    cubealphatexture.colorSpace = THREE.SRGBColorSpace;
     const cubeambienttexture = textureLoader.load('/src/images/11-materials/static/textures/door/ambientOcclusion.jpg');
+    cubeambienttexture.colorSpace = THREE.SRGBColorSpace;
     const cubeheighttexture = textureLoader.load('/src/images/11-materials/static/textures/door/height.jpg');
+    cubeheighttexture.colorSpace = THREE.SRGBColorSpace;
     const cubemetalnessTexture = textureLoader.load('/src/images/11-materials/static/textures/door/metalness.jpg');
+    cubemetalnessTexture.colorSpace = THREE.SRGBColorSpace;
     const cubenormaltexture = textureLoader.load('/src/images/11-materials/static/textures/door/normal.jpg');
+    cubenormaltexture.colorSpace = THREE.SRGBColorSpace;
     const cuberoughnesstexture = textureLoader.load('/src/images/11-materials/static/textures/door/roughness.jpg');
+    cuberoughnesstexture.colorSpace = THREE.SRGBColorSpace;
     const cubematcaptexture = textureLoader.load('/src/images/11-materials/static/textures/matcaps/1.png');
-    const cubegradtexture = textureLoader.load('/src/images/11-materials/static/textures/gradients/3.jpg');
-    // MeshPhysicalMaterial for better texture stuff. and different map names
+    cubematcaptexture.colorSpace = THREE.SRGBColorSpace;
+    const cubegradtexture = textureLoader.load('/src/images/11-materials/static/textures/gradients/5.jpg');
+    cubegradtexture.colorSpace = THREE.SRGBColorSpace;
+    // MeshPhysicalMaterial for better texture stuff. and different map names. not great for performance though
     const material = new THREE.MeshPhysicalMaterial({ 
         map: cubecolortexture,
         alphaMap: cubealphatexture,
@@ -81,9 +93,14 @@ function Shapes() {
         
 
      });
+    //  material.clearcoat = 0.5;
+    //  material.clearcoatRoughness = 0.5;
+    material.iridescence = 1;
+    material.iridescenceIOR = 1;
+    material.iridescenceThicknessRange = [100, 800];
+     material.side = THREE.DoubleSide; // requires more prosessing power
+     const cube = new THREE.Mesh(geometry, material);
      
-     material.colorSpace = THREE.SRGBColorSpace;
-    const cube = new THREE.Mesh(geometry, material);
     
 
     // Sphere with texture
@@ -92,8 +109,12 @@ function Shapes() {
     sphereTexture.colorSpace = THREE.SRGBColorSpace;
 
     const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
-    const sphereMaterial = new THREE.MeshBasicMaterial({ map: sphereTexture });
-    sphereMaterial.transparent = true;
+    const sphereMaterial = new THREE.MeshToonMaterial({ map: sphereTexture  });
+    cubegradtexture.minFilter = THREE.NearestFilter;
+    cubegradtexture.magFilter = THREE.NearestFilter;
+    cubegradtexture.generateMipmaps = false; // better for gpu
+    sphereMaterial.gradientMap = cubegradtexture;
+    sphereMaterial.transparent = false;
     sphereMaterial.opacity = 0.5;
     const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
     sphere.position.x = 2; // move it so it doesn’t overlap the cube
@@ -115,10 +136,28 @@ function Shapes() {
     torusRoughness.colorSpace = THREE.SRGBColorSpace;
 
     const torusGeometry = new THREE.TorusGeometry(.7, 0.2, 15, 100);
-    const torusMaterial = new THREE.MeshBasicMaterial({ map: torusTexture });
+    const torusMaterial = new THREE.MeshPhongMaterial({ 
+        map: torusTexture,
+        normalMap: torusNormal,
+
+     });
+     
+     torusMaterial.shininess = 70;
+    //  torusMaterial.sheenColor.set(1,1,1);
+     torusMaterial.specular = new THREE.Color('grey');
+    torusMaterial.colorSpace = THREE.SRGBColorSpace;
     const torus = new THREE.Mesh(torusGeometry, torusMaterial);
     torus.position.x = -2;
-    scene.add(torus, sphere, cube);
+
+    const coneGeometry = new THREE.ConeGeometry(1, 2, 32);
+     const cone = new THREE.Mesh(coneGeometry, material);
+     cone.position.x = 2;
+     cone.position.y = 2;
+     
+     
+
+
+    scene.add(torus, sphere, cube, cone);
 
     // Debug controls
 const cubeParams = {
@@ -142,6 +181,13 @@ cubeFolder.addColor(cubeParams, 'color').onChange((val) => {
 cubeFolder.add(cube.position, 'x').min(-3).max(3).step(0.01).name('posX');
 cubeFolder.add(cube.position, 'y').min(-3).max(3).step(0.01).name('posY');
 cubeFolder.add(cube.position, 'z').min(-3).max(3).step(0.01).name('posZ');
+cubeFolder.add(material, 'roughness').min(0).max(1).step(0.1).name('roughness');
+cubeFolder.add(material, 'metalness').min(0).max(1).step(0.1).name('metalness');
+cubeFolder.add(material, 'iridescence').min(0).max(1).step(0.0001).name('iridescence');
+cubeFolder.add(material, 'iridescenceIOR').min(1.0).max(2.333).step(0.0001).name('iridescenceIOR');
+cubeFolder.add(material.iridescenceThicknessRange, '0').min(1).max(1000).step(1);
+cubeFolder.add(material.iridescenceThicknessRange, '1').min(1).max(1000).step(1);
+cubeFolder.add(material, 'transmission').min(0).max(1).step(0.0001).name('transmission');
 cubeFolder.add(cubeParams, 'rotationSpeed').min(0.1).max(5).step(0.1).name('rot/sec');
 cubeFolder.add(cubeParams, 'spin');
 
@@ -153,6 +199,13 @@ cubeFolder.add(cubeParams, 'spin');
     // Add ambient light
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
     scene.add(light, ambientLight);
+
+    const rgbeLoader = new HDRLoader()
+    rgbeLoader.load('src/images/11-materials/static/textures/environmentMap/2k.hdr', (environmentMap) => {
+        environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+        scene.background = environmentMap;
+        scene.environment = environmentMap;
+    })
 
     // Resize handler
     const handleResize = () => {
